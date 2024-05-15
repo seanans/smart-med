8c36426d127d152e8e6ddff1beec7d3657a562d8
seanans <maks1998ivan@gmail.com>
2024-05-15 20:03:08 +0300

    Program.cs update

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Text;
 
-Console.WriteLine("Hello, World!");
+namespace SmartMed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            // IUserService userService = new UserService();
+            // IPatientService patientService = new PatientService();
+            // IDoctorService doctorService = new DoctorService();
+            //
+            // Menu menu = new Menu(userService, patientService, doctorService);
+            // menu.DisplayMainMenu();
+        }
+    }
+}
